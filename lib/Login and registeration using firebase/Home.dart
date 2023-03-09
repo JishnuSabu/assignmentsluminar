2ad1346f3d2b56cab165d8868609792a81a47197
VishnuSabu97 <vishnukunnumpurath1@gmail.com>
2023-03-09 17:34:57 +0530

    commit

---
+++ lib/Login and registeration using firebase/Home.dart
@@ -0,0 +1,26 @@
+import 'package:assignmentsluminar/Login%20and%20registeration%20using%20firebase/Authentication.dart';
+import 'package:assignmentsluminar/Login%20and%20registeration%20using%20firebase/main.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(
+        child: Text('Welcome'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          AuthenticationHelper()
+              .signOut()
+              .then((_) => Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (contex) => MyAppLogin()),
+                  ));
+        },
+        tooltip: 'Logout',
+        child: const Icon(Icons.logout),
+      ),
+    );
+  }
+}
